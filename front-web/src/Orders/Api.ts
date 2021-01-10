ba584144be5e3f3438f1ab9a71a83690c6aa6057
Marcelo Ponciano <marceloponciano.eb@gmail.com>
2021-01-10 15:23:36 -0300

    Finish front-web

--- front-web/src/Orders/Api.ts
+++ front-web/src/Orders/Api.ts
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { OrderPayload } from "./types";
 
 const API_URL = 'http://localhost:9090'
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
@@ -10,4 +11,8 @@ export const fetchProducts = () => {
 
 export const fetchLocalMapBox = (local: string) => {
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
+
+export const saveOrder = (payload: OrderPayload) => {
+    return axios.post(`${API_URL}/orders`, payload);
 }
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,29 +1,72 @@
 import { useEffect, useState } from 'react';
-import { fetchProducts } from './Api';
+import { toast } from 'react-toastify';
+import Footer from '../Footer';
+import { fetchProducts, saveOrder } from './Api';
+import { checkIsSelected } from './helpers';
 import OrderLocation from './OrderLocation';
+import OrdersSummary from './OrdersSummary';
 import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
-import './styles.css';
 import { OrderLocationData, Product } from './types';
+import './styles.css';
 
 function Orders() {
 
-    const [products, setProducts] = useState<Product[]>([]);
-
+    const [products, setProducts] = useState<Product[]>([])
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([])
     const [orderLocation, setOrderLocation] = useState<OrderLocationData>()
+    const totalPrice = selectedProducts.reduce((sum, item) => {
+            return sum + item.price;
+    }, 0);
 
     useEffect(() => {
         fetchProducts()
             .then(response => setProducts(response.data))
-            .catch(error => console.log(error));
+            .catch(() => toast.warning('Erro ao listar produtos.'));
     }, [])
 
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts, product)
+      
+        if (isAlreadySelected) {
+          const selected = selectedProducts.filter(item => item.id !== product.id);
+          setSelectedProducts(selected);
+        } else {
+          setSelectedProducts(previous => [...previous, product]);
+        }
+      }
+
+    const handleSubmit = () => {
+    const productsIds = selectedProducts.map(({ id }) => ({ id }));
+    const payload = {
+        ...orderLocation!,
+        products: productsIds
+    }
+    
+    saveOrder(payload).then((response) => {
+        toast.error(`Pedido enviado com sucesso! Nº ${response.data.id}`);
+        setSelectedProducts([]);
+    })
+        .catch(() => {
+        toast.warning('Erro ao enviar pedido');
+        })
+    }
+
     return(
-     <div className="orders-container" >
+        <>
+         <div className="orders-container" >
          <StepsHeader />
-         <ProductsList products={products}/>
+         <ProductsList products={products}
+                        onSelectProduct={handleSelectProduct}
+                        selectedProducts={selectedProducts}
+                        />
          <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
-     </div>
+         <OrdersSummary amount={selectedProducts.length}
+                        totalPrice={totalPrice}
+                        onSubmit={handleSubmit} />
+        </div>
+        <Footer />
+        </>
     );
 }
 
